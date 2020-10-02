bfdc6d0579333b37c5d35c7eb40b905539812f8b
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:55:49 -0300

    Update Código para Verificação de Triângulos.c

--- Código para Verificação de Triângulos.c
+++ Código para Verificação de Triângulos.c
@@ -4,20 +4,20 @@ int main(void) {
 
 float e_lado1, e_lado2, e_lado3; //declara as variáveis
 
+printf("Insira o tamanho do lado 1: ");//instrui o usuário
 scanf("%f",&e_lado1); //lê o valor da variável 
-printf("lado 1 = %f\n",e_lado1); //mostra o valor do lado 1
 
 //repetir o mesmo processo com as demais variáveis:
 
+printf("Insira o tamanho do lado 2: ");
 scanf("%f",&e_lado2);
-printf("lado 2 = %f\n",e_lado2); 
- 
+
+printf("Insira o tamanho do lado 3: "); 
 scanf("%f",&e_lado3);
-printf("lado 3 = %f\n",e_lado3);
 
 //verficar se os valores apresentados resultam de fato em um triângulo conforme a condição de existência:
 
-if(e_lado1 > e_lado2 + e_lado3 || e_lado2 > e_lado1 + e_lado3 || e_lado3 > e_lado1 + e_lado2 || e_lado1 == 0 || e_lado2 ==0 ||e_lado3 ==0){
+if(e_lado1 > e_lado2 + e_lado3 || e_lado2 > e_lado1 + e_lado3 || e_lado3 > e_lado1 + e_lado2 || e_lado1 == 0 || e_lado2 == 0 || e_lado3 == 0 || e_lado1 < 0 || e_lado2 < 0 || e_lado3 < 0){
 printf("não é um triâgulo");
 }
 
