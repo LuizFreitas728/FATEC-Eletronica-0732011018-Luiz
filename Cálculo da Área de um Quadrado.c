f0e4f1f873e29b72a8f1b5eb76c416b6552eb9cb
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:42:29 -0300

    Update Cálculo da Área de um Quadrado.c

--- Cálculo da Área de um Quadrado.c
+++ Cálculo da Área de um Quadrado.c
@@ -5,11 +5,11 @@ int main(void) {
 
 float e_lado, s_area; //declara as variáveis
 
+printf("Digite o valor do lado do quadrado: ");//intrução para o usuário
 scanf("%f",&e_lado); //lê o valor do lado do quadrado
-printf("lado do quadrado:%f\n", e_lado); //mostra o valor lido na tela
 
 s_area = pow(e_lado, 2); //executa a operação
-printf("area do quadrado:%f\n", s_area); //mostra o resultado na tela
+printf("\nÁrea do quadrado: %f\n", s_area); //mostra o resultado na tela
 
 return 0;
 }
