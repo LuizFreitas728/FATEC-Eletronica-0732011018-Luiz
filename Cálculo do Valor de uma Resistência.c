c43291afa77c2b429a5a7f36c6a004ba6452bee9
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:49:15 -0300

    Update Cálculo do Valor de uma Resistência.c

--- Cálculo do Valor de uma Resistência.c
+++ Cálculo do Valor de uma Resistência.c
@@ -2,18 +2,18 @@
 
 int main(void) {
 
-float e_tensao, e_corrente, s_resistencia; //declara as variáveis
+double e_tensao, e_corrente, s_resistencia; //declara as variáveis
 
-scanf("%f",&e_tensao); 
-printf("Valor da tensão: %f\n",e_tensao);
+printf("Insira o valor da tensão elétrica: ");
+scanf("%lf",&e_tensao); 
 
-scanf("%f",&e_corrente);
-printf("Valor da corrente: %f\n",e_corrente);
-//lê e mostra na tela os valores lidos
+printf("Insira o valor da corrente elétrica: ");
+scanf("%lf",&e_corrente);
+//instrui o usuário lê os valores
 
 s_resistencia = e_tensao/ e_corrente; //executa a operação
 
-printf("Valor da resistência: %f\n", s_resistencia); //mostra o resultado na tela
+printf("\nValor da resistência: %lf Ω", s_resistencia); //mostra o resultado na tela
 
 return 0;
 }
