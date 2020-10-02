e674010f82fd7adbe45c1eb1acaaee9b80112efd
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:44:15 -0300

    Update Cálculo da Área de um Retângulo.c

--- Cálculo da Área de um Retângulo.c
+++ Cálculo da Área de um Retângulo.c
@@ -4,19 +4,19 @@ int main(void) {
 
 float e_lado1, e_lado2, s_area; //declara as variáveis
 
-scanf("%f",&e_lado1); //lê o valor do comprimento do retângulo
-printf("comprimento: %f\n", e_lado1); //mostra o valor lido na tela  
+printf("Digite o comprimento: "); //intrução ao usuário
+scanf("%f",&e_lado1); //lê o valor do comprimento do retângulo 
 
+printf("Digite a altura: "); //intrução ao usuário
 scanf("%f",&e_lado2); //lê o valor da altura do retângulo
-printf("altura: %f\n", e_lado2); //mostra o valor lido na tela
 
-if(e_lado1 == e_lado2 || e_lado1== 0 || e_lado2 == 0){
+if(e_lado1 == e_lado2 || e_lado1== 0 || e_lado2 == 0 || e_lado1 < 0 ||e_lado2 < 0){
 printf("não é um retâgulo");
 } // verifica e mostra na tela se de fato os valores lidos resultam num retâgulo
 
 else{
 s_area = e_lado1*e_lado2; //excuta a operação
-printf("Área do retângulo: %f\n", s_area); //mostra na tela a área do retângulo
+printf("\nÁrea do retângulo: %f", s_area); //mostra na tela a área do retângulo
 }
 
 return 0;
