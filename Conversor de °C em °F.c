6cae4d4f4007e22b4c06af39b9e453bac5b8b303
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:28:07 -0300

    Update Conversor de °C em °F.c

--- Conversor de °C em °F.c
+++ Conversor de °C em °F.c
@@ -2,13 +2,13 @@
 
 int main(void) {
 
-float e_graus_celsius, s_graus_farenheit; //declara as variáveis
+double e_graus_celsius, s_graus_farenheit; //declara as variáveis
 
-scanf("%f",&e_graus_celsius); //lê o a temperatura em °C
-printf("Valor em °C: %f\n", e_graus_celsius); //mostra na tela
+printf("Insira um valor de temperatura em °C: ");
+scanf("%lf",&e_graus_celsius); //lê o a temperatura em °C
 
 s_graus_farenheit = (9*e_graus_celsius + 160)/ 5; //executa a operação
-printf("Valor em °F: %f\n", s_graus_farenheit); //mostra o resultado na tela
+printf("\nValor da temperatura inserida em °F: %lf", s_graus_farenheit); //mostra o resultado na tela
 
 return 0;
 }
