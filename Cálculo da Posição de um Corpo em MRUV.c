e7bd69a6f8963a208bd0ac436327d3e124b30da2
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:40:47 -0300

    Update Cálculo da Posição de um Corpo em MRUV.c

--- Cálculo da Posição de um Corpo em MRUV.c
+++ Cálculo da Posição de um Corpo em MRUV.c
@@ -3,38 +3,49 @@
 
 int main(void) {
 
-float e_posicao_inicial, e_velocidade_inicial, e_tempo, e_aceleracao, s_posicao_final; //declara as variáveis
+double e_posicao_inicial, e_velocidade_inicial, e_tempo, e_aceleracao, s_posicao_final; //declara as variáveis
 
-printf("Os valores a serem apresentados não levam em consideração as unidades"); //alerta para o usuário do programa
+printf("ATENÇÃO: Os valores a serem apresentados não levam em consideração as unidades \n \n"); //alerta para o usuário do programa
 
-scanf("%f",&e_tempo); //Lê a entrada de tempo
+printf("Digite o instante do movimento a ser analisado: ");
+scanf("%lf",&e_tempo); //Lê a entrada de tempo
 
 if(e_tempo < 0){
 printf("Tempo não pode ser negativo"); 
 } //verifica se a variável tempo está em sua condição de existência
 
 else{
-printf("Instante do movimento: %f\n", e_tempo);
-
-scanf("%f",&e_aceleracao);
+printf("Digite a aceleração do corpo em questão: ");
+scanf("%lf",&e_aceleracao);
 
 if(e_aceleracao == 0){
 printf("Isso não é um MRUV");  
 } //verifica se de fato o corpo está em MRUV
 
 else{
-printf("Aceleração do corpo: %f\n",e_aceleracao);
-//introduz e mostra na tela outros valores: 
+printf("Digite a velocidade inicial do corpo: ");
+scanf("%lf",&e_velocidade_inicial);
 
-scanf("%f",&e_posicao_inicial);
-printf("Posição inicial do corpo: %f\n",e_posicao_inicial);
+double velocidade_em_km_por_hora = e_velocidade_inicial*3.6;
 
-scanf("%f",&e_velocidade_inicial);
-printf("Velocidade inicial do corpo: %f\n",e_velocidade_inicial);
+if(e_velocidade_inicial > 299792458 || velocidade_em_km_por_hora > 1079252848.8){
+printf("\nSomente as partículas neutrino conseguem ultrapassar a velocidade da luz - Epecíficamente no meio aquoso. De todo modo, ainda sim essas partículas não excedem essa velocidade em grandes proporções. Se o corpo em movimento estiver no vácuo e não for essa partícula, a velocidade inserida é inválida.");  
+}
+
+else{
+if(e_velocidade_inicial > 277777 || velocidade_em_km_por_hora > 1000000){
+printf("\nOberve que um corpo que possua uma velocidade dessas proporções necessáriamente possui uma massa muito pequena, conforme propõe a teria da relatividade");
+} //Aplica as constatações propostas por Albert Einstein em sua teoria da relatividade
+
+else{
+printf("Digite a posição inicial do corpo: ");
+scanf("%lf",&e_posicao_inicial);
 
 s_posicao_final = e_posicao_inicial + e_velocidade_inicial*e_tempo + 
 ((e_aceleracao*pow(e_tempo, 2))/ 2); //executa a operação
-printf("Posição final do corpo: %f\n", s_posicao_final); //mostra o resultado na tela
+printf("\nPosição final do corpo: %lf", s_posicao_final); //mostra o resultado na tela
+}
+}
 }
 }
 
