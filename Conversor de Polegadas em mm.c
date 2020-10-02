52a70c313f9678ddeeadb66cefeecf628194d7d3
LuizFreitas728 <70214899+LuizFreitas728@users.noreply.github.com>
2020-10-02 16:26:40 -0300

    Update Conversor de Polegadas em mm.c

--- Conversor de Polegadas em mm.c
+++ Conversor de Polegadas em mm.c
@@ -2,13 +2,13 @@
 
 int main(void) {
 
-float e_valor_em_polegada, s_valor_em_mm; //declara as variáveis
+double e_valor_em_polegada, s_valor_em_mm; //declara as variáveis
 
-scanf("%f",&e_valor_em_polegada); //Lê a variável de entrada
-printf("valor de entrada:%f\n", e_valor_em_polegada); //Motra o valor da variável de entrada na tela
+printf("Digite o valor em polegada: "); //intrução ao usuário
+scanf("%lf",&e_valor_em_polegada); //Lê a variável de entrada
 
 s_valor_em_mm = e_valor_em_polegada*25.4; //executa a operação
-printf("valor de saída:%f\n", s_valor_em_mm); //mostra o resultado na tela
+printf("\nValor em mm: %lf", s_valor_em_mm); //mostra o resultado na tela
 
 return 0;
 }
